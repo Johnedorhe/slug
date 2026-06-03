b9915b5bb940817d24d28e7edc5d42e0bbc66cb0
John <cooljohne@gmail.com>
2026-06-03 23:30:54 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Slug Example",
+  title: "Dynamic Routing Example",
   description: "A Next.js demo showing dynamic slug routing and example slug pages.",
 };
 
@@ -27,7 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header className="text-yellow-300">
+          <Link className="p-4" href="/">
+            Home
+          </Link>
+          </header>
+        {children}
+        </body>
     </html>
   );
 }
